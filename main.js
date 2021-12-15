471905cad60ce89710da55f9cf4421e3b98fa26e
LincolnProenca <lincoln22220@gmail.com>
2021-12-15 11:54:34 -0300

    Fix button and navbar delay

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ button = document.getElementById("on/off");
 button.onclick = function() {on_off()};
 
 bodyStyles = document.body.style;
-bodyStyles.setProperty('--color','#2196f3')
+bodyStyles.setProperty('--color','#1149e4')
 
 function on_off(){
     var element = document.getElementById("button")
@@ -11,19 +11,25 @@ function on_off(){
             element.innerHTML = "on";
             button.classList.add('on');
             button.classList.remove('off');
+            button.classList.add('turning');
             setTimeout(function() {
                 button.classList.add("on_finish");
                 button.classList.remove('on');
+                button.classList.remove('turning');
                 document.getElementById("nav-bar").classList.add("on_finish");
             }, 1200);
             break;
         case "on":
-            element.innerHTML = "off";
-            console.log(button.id);
-            button.classList.add('off');
-            button.classList.remove('on_finish');
-            document.getElementById("nav-bar").classList.remove('on_finish');
-            break;
+            if(button.classList.contains("turning")){
+                break;
+            }else{
+                element.innerHTML = "off";
+                console.log(button.id);
+                button.classList.add('off');
+                button.classList.remove('on_finish');
+                document.getElementById("nav-bar").classList.remove('on_finish');
+                break;
+            }
         default:
             console.log("sexo");
     }
