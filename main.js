c6afa0639170d34074a731c1c0474c8abd27d824
LincolnProenca <91763571+LincolnProenca@users.noreply.github.com>
2021-12-14 22:26:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,18 @@ function on_off(){
             element.innerHTML = "on";
             button.classList.add('on');
             button.classList.remove('off');
+            setTimeout(function() {
+                button.classList.add("on_finish");
+                button.classList.remove('on');
+                document.getElementById("nav-bar").classList.add("on_finish");
+            }, 1200);
             break;
         case "on":
             element.innerHTML = "off";
             console.log(button.id);
             button.classList.add('off');
-            button.classList.remove('on');
+            button.classList.remove('on_finish');
+            document.getElementById("nav-bar").classList.remove('on_finish');
             break;
         default:
             console.log("sexo");
